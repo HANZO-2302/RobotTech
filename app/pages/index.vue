7d42ae9a6a919b9164d22c8f5266b02639e0e418
User_PC <m2302835@gmail.com>
2026-09-15 23:06:08 +0300

    Add SVG icons for ChatGPT and Claude

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,25 +19,25 @@ const modules = [Pagination, Navigation, A11y, Parallax, EffectCreative];
 
 const speakers = [
     {
-    src: "/Elon Musk.png",
+    src: "/Elon Musk2.png",
     alt: "Elon Musk",
     robot: "/Optimus.png",
     robotAlt: "Optimus",
   },
     {
-    src: "/Elon Musk.png",
-    alt: "Elon Musk",
-    robot: "/Optimus.png",
-    robotAlt: "Optimus",
+    src: "/Sam Altman.png",
+    alt: "Sam Altman",
+    robot: "/chatgpt.svg",
+    robotAlt: "OpenAI",
   },
   {
-    src: "/Elon Musk.png",
-    alt: "Elon Musk",
-    robot: "/Optimus.png",
-    robotAlt: "Optimus",
+    src: "/Dario Amodei.png",
+    alt: "Dario Amodei",
+    robot: "/claude.svg",
+    robotAlt: "Anthropic",
   },
   {
-    src: "/Marc Raibert.png",
+    src: "/Marc Raibert1.png",
     alt: "Marc Raibert",
     robot: "/Atlas.png",
     robotAlt: "Atlas",
@@ -95,7 +95,7 @@ const cardVariants = {
 <template>
   <!-- <div class="pointer-events-none absolute inset-0 z-0 bg-[linear-gradient(to_right,rgba(255,255,255,0.08)_1px,transparent_1px),linear-gradient(to_bottom,rgba(255,255,255,0.06)_1px,transparent_1px)] bg-[size:45px_45px]"
  /> -->
-  <div class="relative flex flex-1 flex-col items-center justify-center">
+  <div class="relative flex flex-1 flex-col items-center justify-center overscroll-auto">
     <!-- Background -->
     <div
       class="pointer-events-none absolute right-0 bottom-0 z-0 w-full sm:w-auto"
@@ -139,9 +139,9 @@ const cardVariants = {
       </motion.div>
 
       <!-- Decktop -->
-      <div class="relative my-10 max-w-7xl w-full hidden sm:flex px-8">
+      <div class="relative my-6 max-w-7xl w-full hidden sm:flex px-8">
         <div
-          class="relative flex w-full items-start justify-center gap-8"
+          class="relative flex w-full items-start justify-center gap-x-[3%] "
         >
           <motion.div
             v-for="(speaker, index) in speakers"
@@ -151,20 +151,20 @@ const cardVariants = {
             initial="hidden"
             whileInView="visible"
             :viewport="{ once: true, amount: 0.25 }"
-            class="relative h-auto w-1/6 flex flex-col items-start justify-start mx-auto"
+            class="relative h-auto w-full flex flex-col items-start justify-start mx-auto "
           >
             <div
-              class="relative flex flex-col items-center justify-center -skew-x-[19deg] max-w-[60vw] h-full w-full gap-1 mx-auto"
+              class="relative flex flex-col items-center justify-center -skew-x-[19deg] h-full w-full gap-y-1 mx-auto "
             >
               <!-- Вверхняя панель -->
               <div
                 class="relative aspect-[4/3] flex justify-center h-full w-full gap-1 "
               >
                 <div
-                  class="relative w-[14%] rounded-[1.5px] bg-gradient-to-b from-[#60827d] to-[#246057]"
+                  class="relative w-[20%] rounded-[1.5px] bg-gradient-to-b from-[#60827d] to-[#246057]"
                 >
                   <div
-                    class="absolute bottom-2 left-0 right-0 text-[clamp(0.5rem,1vw,0.7rem)] font-roboto font-thin text-white/80 text-nowrap -rotate-90 -skew-x-[19deg]"
+                    class="absolute flex items-center justify-start bottom-[10%] left-0 right-0 text-[clamp(2px,1vw,12px)] font-roboto font-thin text-white/80 text-nowrap -rotate-90 -skew-x-[19deg]"
                   >
                     {{ speaker.alt }}
                   </div>
@@ -175,13 +175,13 @@ const cardVariants = {
                   <NuxtImg
                     :src="speaker.src"
                     :alt="speaker.alt"
-                    class="absolute object-cover h-full w-full right-[0.5vw] -bottom-0 skew-x-[19deg]"
+                    class="absolute object-cover h-full w-full right-[0.4vw] -bottom-0 skew-x-[19deg]"
                   />
                 </div>
               </div>
               <!-- Нижняя панель -->
               <div
-                class="relative aspect-[4/3] left-[14%] h-full w-full flex justify-center gap-1"
+                class="relative aspect-[4/3] left-[19%] h-full w-full flex justify-center gap-1"
               >
                 <div
                   data-swiper-parallax-y="300"
@@ -190,15 +190,15 @@ const cardVariants = {
                   <NuxtImg
                     :src="speaker.robot"
                     :alt="speaker.robotAlt"
-                    class="absolute object-cover h-[90%] w-full right-[0.5vw] -bottom-0 skew-x-[19deg]"
+                    class="absolute object-cover h-[90%] w-full -bottom-0 skew-x-[19deg]"
                   />
                 </div>
                 <div
                   data-swiper-parallax-y="-1200"
-                  class="relative w-[14%] h-full rounded-[1.5px] bg-gradient-to-t from-[#60827d] to-[#246057]"
+                  class="relative w-[20%] h-full rounded-[1.5px] bg-gradient-to-t from-[#60827d] to-[#246057]"
                 >
                   <div
-                    class="absolute bottom-2 left-0 right-0 text-[clamp(0.5rem,1vw,0.7rem)] font-roboto text-white/80 text-nowrap -skew-x-[19deg] -rotate-90"
+                    class="absolute bottom-[10%] left-0 right-0 text-[clamp(2px,1vw,12px)] font-roboto text-white/80 text-nowrap -skew-x-[19deg] -rotate-90"
                   >
                     {{ speaker.robotAlt }}
                   </div>
